0fae035e022fc425f3578205ce093deb1bd64a10
RVR2D2 <poompeii@gmail.com>
2021-10-12 22:46:49 +0600

    feat: save accessToke

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,14 @@
 
 <script>
 import McvTopBar from '@/components/TopBar'
+import {actionTypes} from '@/store/modules/auth'
 
 export default {
 	components: {
 		McvTopBar
 	},
+	mounted() {
+	this.$store.dispatch(actionTypes.getCurrentUser)
+	}
 }
 </script>
