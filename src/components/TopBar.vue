db09e919c9dc92c4ac8afd25d42aa9bdb632b8ac
RVR2D2 <poompeii@gmail.com>
2021-10-12 21:52:53 +0600

    feat: add getters

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -49,7 +49,7 @@
 						</router-link>
 					</li>
 				</template>
-				<template v-if="!isLoggedIn">
+				<template v-if="isAnonymous">
 					<li class="nav-item">
 						<router-link
 							class="nav-link"
@@ -75,19 +75,17 @@
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import {getterTypes} from '@/store/modules/auth'
+import {mapGetters} from 'vuex'
 
 export default {
 	name: "McvTopBar",
 	computed: {
-		...mapState({
-			currentUser: state => state.auth.currentUser,
-			isLoggedIn: state => state.auth.isLoggedIn
-		})
+		...mapGetters({
+			currentUser: getterTypes.currentUser,
+			isLoggedIn: getterTypes.isLoggedIn,
+			isAnonymous: getterTypes.isAnonymous
+		}),
 	}
 }
 </script>
-
-<!--<style scoped>-->
-
-<!--</style>-->
