7fa9e66042118aa25bd7101d1ef5330b2cbd0513
RVR2D2 <poompeii@gmail.com>
2021-10-13 22:33:43 +0600

    feat: add component feed, add api connect

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -3,14 +3,14 @@
 		<div class="container">
 			<router-link
 				class="navbar-brand"
-				:to="{name: 'home'}">
+				:to="{name: 'globalFeed'}">
 				Medium clone
 			</router-link>
 			<ul class="nav navbar-nav pull-xs-right">
 				<li class="nav-item">
 					<router-link
 						class="nav-link"
-						:to="{name: 'home'}"
+						:to="{name: 'globalFeed'}"
 						active-class="active"
 						exact
 					>
