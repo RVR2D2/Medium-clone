4c444a63bd9667be6eb58a8a445a3371cc64fa26
RVR2D2 <poompeii@gmail.com>
2021-10-18 23:00:40 +0600

    feat: add component Loading, Banner, ErrorMessage

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,115 +1,128 @@
 <template>
-	<div class="feed">
-		<div v-if="isLoading" class="feed__loading">
-			<img src="../assets/loading.svg" alt="loading">
-		</div>
-		<div v-if="error">Something bad happened</div>
-		<div v-if="feed">
-			<div class="article-preview" v-for="(article, index) in feed.articles" :key="index">
-				<div class="article-meta">
-					<router-link :to="{
-						name: 'userProfile', params: {slug: article.author.username}}">
-						<img :src="article.author.image" alt="user"/>
-					</router-link>
-					<div class="info">
-						<router-link
-							:to="{
-							name: 'userProfile',
-							params: {slug: article.author.username}
-						}"
-							class="author"
-						>
-							{{ article.author.username }}
-						</router-link>
-						<span class="date">{{ article.createdAt }}</span>
-					</div>
-					<div class="pull-xs-right">
-						ADD TO FAVORITES
-					</div>
-				</div>
-				<router-link :to="{name: 'article', params: {slug: article.slug}}" class="preview-link">
-					<h1>{{ article.title }}</h1>
-					<p>{{ article.description }}</p>
-					<span>Read more...</span>
-					TAG LIST
-				</router-link>
-			</div>
-			<McvPagination
-				:total="feed.articlesCount"
-				:limit="limit"
-				:currentPage="currentPage"
-				:url="baseUrl"
-			/>
-		</div>
-	</div>
+  <div class="feed">
+    <McvLoading v-if="isLoading" />
+    <McvError v-if="error" />
+    <div v-if="feed">
+      <div
+        class="article-preview"
+        v-for="(article, index) in feed.articles"
+        :key="index"
+      >
+        <div class="article-meta">
+          <router-link
+            :to="{
+              name: 'userProfile',
+              params: { slug: article.author.username }
+            }"
+          >
+            <img :src="article.author.image" alt="user" />
+          </router-link>
+          <div class="info">
+            <router-link
+              :to="{
+                name: 'userProfile',
+                params: { slug: article.author.username }
+              }"
+              class="author"
+            >
+              {{ article.author.username }}
+            </router-link>
+            <span class="date">{{ article.createdAt }}</span>
+          </div>
+          <div class="pull-xs-right">
+            ADD TO FAVORITES
+          </div>
+        </div>
+        <router-link
+          :to="{ name: 'article', params: { slug: article.slug } }"
+          class="preview-link"
+        >
+          <h1>{{ article.title }}</h1>
+          <p>{{ article.description }}</p>
+          <span>Read more...</span>
+          TAG LIST
+        </router-link>
+      </div>
+      <McvPagination
+        :total="feed.articlesCount"
+        :limit="limit"
+        :currentPage="currentPage"
+        :url="baseUrl"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import {mapState} from 'vuex'
-import {actionTypes} from '@/store/modules/feed'
-import McvPagination from '@/components/Pagination'
-import {limit} from '@/helpers/vars'
-import {stringify, parseUrl} from 'query-string'
+import { mapState } from "vuex";
+import { actionTypes } from "@/store/modules/feed";
+import McvPagination from "@/components/Pagination";
+import { limit } from "@/helpers/vars";
+import { stringify, parseUrl } from "query-string";
+import McvLoading from "@/components/Loading";
+import McvError from "@/components/Error";
 
 export default {
-	name: "McvFeed",
-	components: {
-		McvPagination
-	},
-	props: {
-		apiUrl: {
-			type: String,
-			required: true
-		}
-	},
-	data() {
-		return {
-			limit,
-			url: '/'
-		}
-	},
-	computed: {
-		...mapState({
-			isLoading: state => state.feed.isLoading,
-			feed: state => state.feed.data,
-			error: state => state.feed.error
-		}),
-		currentPage() {
-			return Number(this.$route.query.page || '1  ')
-		},
-		baseUrl() {
-			return this.$route.path
-		},
-		offset() {
-			return this.currentPage * limit - limit
-		}
-	},
-	watch: {
-		currentPage() {
-			this.fetchFeed()
-		}
-	},
-	mounted() {
-		this.fetchFeed()
-	},
-	methods: {
-		fetchFeed() {
-			const parsedUrl = parseUrl(this.apiUrl)
-			const stringifiedParams = stringify({
-				limit,
-				offset: this.offset,
-				...parsedUrl.query
-			})
-			const apiUrlWithParams = `${parsedUrl.url}?${stringifiedParams}`
-			this.$store.dispatch(actionTypes.getFeed, {apiUrl: apiUrlWithParams})
-		}
-	}
-}
+  name: "McvFeed",
+  components: {
+    McvPagination,
+    McvLoading,
+    McvError
+  },
+  props: {
+    apiUrl: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      limit,
+      url: "/"
+    };
+  },
+  computed: {
+    ...mapState({
+      isLoading: state => state.feed.isLoading,
+      feed: state => state.feed.data,
+      error: state => state.feed.error
+    }),
+    currentPage() {
+      return Number(this.$route.query.page || "1  ");
+    },
+    baseUrl() {
+      return this.$route.path;
+    },
+    offset() {
+      return this.currentPage * limit - limit;
+    }
+  },
+  watch: {
+    currentPage() {
+      this.fetchFeed();
+    }
+  },
+  mounted() {
+    this.fetchFeed();
+  },
+  methods: {
+    fetchFeed() {
+      const parsedUrl = parseUrl(this.apiUrl);
+      const stringifiedParams = stringify({
+        limit,
+        offset: this.offset,
+        ...parsedUrl.query
+      });
+      const apiUrlWithParams = `${parsedUrl.url}?${stringifiedParams}`;
+      this.$store.dispatch(actionTypes.getFeed, { apiUrl: apiUrlWithParams });
+    }
+  }
+};
 </script>
 
 <style scoped>
 .feed__loading {
-	display: flex;
-	justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 </style>
--- src/components/PopularTags.vue
+++ src/components/PopularTags.vue
@@ -1,39 +1,45 @@
 <template>
-	<div>
-		<div v-if="isLoading">Loading...</div>
-		<div v-if="error">Something bad happened</div>
-		<div class="sidebar" v-if="popularTags">
-			<p>Popular Tags</p>
-			<div class="tag-list">
-				<router-link
-					v-for="popularTag in popularTags" :key="popularTag"
-					:to="{name: 'tag', params: {slug: popularTags} }"
-					class="tag-default tag-pill"
-				>{{popularTag}}</router-link>
-			</div>
-		</div>
-	</div>
+  <div>
+    <McvLoading v-if="isLoading" />
+    <McvError v-if="error" />
+    <div class="sidebar" v-if="popularTags">
+      <p>Popular Tags</p>
+      <div class="tag-list">
+        <router-link
+          v-for="popularTag in popularTags"
+          :key="popularTag"
+          :to="{ name: 'tag', params: { slug: popularTags } }"
+          class="tag-default tag-pill"
+          >{{ popularTag }}
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import {mapState} from "vuex";
-import {actionTypes} from "@/store/modules/popularTags";
+import { mapState } from "vuex";
+import { actionTypes } from "@/store/modules/popularTags";
+import McvLoading from "@/components/Loading";
+import McvError from "@/components/Error";
 
 export default {
-	name: "McvPopularTags",
-	computed: {
-		...mapState({
-			isLoading: state => state.popularTags.isLoading,
-			error: state => state.popularTags.error,
-			popularTags: state => state.popularTags.data
-		})
-	},
-	mounted() {
-		this.$store.dispatch(actionTypes.getPopularTags)
-	}
-}
+  name: "McvPopularTags",
+  components: {
+    McvLoading,
+    McvError
+  },
+  computed: {
+    ...mapState({
+      isLoading: state => state.popularTags.isLoading,
+      error: state => state.popularTags.error,
+      popularTags: state => state.popularTags.data
+    })
+  },
+  mounted() {
+    this.$store.dispatch(actionTypes.getPopularTags);
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
